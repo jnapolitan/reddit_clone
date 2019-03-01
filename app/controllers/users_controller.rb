fa1e63099710f7ed494daad57bd0486bca75eb7a
Julian Napolitan <julian.napolitan@gmail.com>
2019-03-01 14:36:26 -0800

    Create Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render :show
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
