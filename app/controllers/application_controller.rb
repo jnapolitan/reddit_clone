49fdd64abaf107c036a8d370808852049faa5b6b
Julian Napolitan <julian.napolitan@gmail.com>
2019-03-01 18:31:16 -0800

    Add application controller session methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
+  helper_method :current_user, :signed_in?
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:token])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def login(user)
+    @current_user = user
+    session[:token] = user.reset_session_token!
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:token] = nil
+  end
+
+  def require_signed_in!
+    redirect_to new_session_url unless signed_in?
+  end
+
+  def require_signed_out!
+    redirect_to user_url(current_user) if signed_in?
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
 
     if @user.save
       login(@user)
-      # redirect_to subs_url
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
