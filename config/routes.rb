7cc12ad1b9d511b61ec6de0509816cea48256c7d
Julian Napolitan <julian.napolitan@gmail.com>
2019-03-12 21:11:44 -0700

    Create routes and controller for comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   resources :subs, except: :destroy
 
-  resources :posts, except: [:index, :destroy]
-  
+  resources :posts, except: [:index, :destroy] do
+    resources :comments, only: [:new]
+  end
+
+  resources :comments, only: [:create]
 end
