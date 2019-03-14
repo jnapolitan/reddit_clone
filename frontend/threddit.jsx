182f4431ce748a82cc122e2af82269a9b0d03d42
Julian Napolitan <julian.napolitan@gmail.com>
2019-03-14 12:55:05 -0700

    Setup webpack and entry file for frontend

---
+++ frontend/threddit.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Cool now react is working.</h1>, root);
+})
